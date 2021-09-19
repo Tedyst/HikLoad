fc19ee752ca20d5971a8d6289de21ac0b75f3702
Stoica Tedy <stoicatedy@gmail.com>
2021-09-19 14:51:48 +0300

    fix: get today's starttime correctly by timezone

--- downloadTodayRecordings.py
+++ downloadTodayRecordings.py
@@ -1,5 +1,5 @@
 import hikvisionapi
-from datetime import datetime
+from datetime import datetime, timezone
 import re
 import os
 import logging
@@ -26,10 +26,12 @@ for channel in channelList['StreamingChannelList']['StreamingChannel']:
     # The channel is a primary channel
     if (int(cid) % 10 == 1):
         # This makes sure that we only get today's recordings
-        starttime = datetime.utcnow().replace(
-            hour=0, minute=0, second=0, microsecond=0).isoformat()
-        endtime = datetime.utcnow().replace(
-            hour=23, minute=59, second=59, microsecond=0).isoformat()
+        starttime = datetime.now().astimezone().replace(
+            hour=0, minute=0, second=0).isoformat()
+        endtime = datetime.now().astimezone().replace(
+            hour=23, minute=59, second=59).isoformat()
+        logging.debug("Using %s and %s as start and end times" %
+                      (starttime, endtime))
 
         recordings = server.Streaming.getPastRecordingsForID(cid, starttime,
                                                              endtime)
@@ -46,8 +48,11 @@ for channel in channelList['StreamingChannelList']['StreamingChannel']:
             url = url.replace(server.host, server.address(
                 protocol=False, credentials=True))
             # You can choose your own filename, this is just an example
-            name = re.sub(r'[-T\:Z]', '', recording['timeSpan']['startTime'])
-            name = name + ".mkv"
+            # This line strips the timezone from the time retuned by the server
+            name = re.sub(r'[-T\:Z]', '', recording['timeSpan']
+                          ['startTime'])
+            # This line appends the camera id and the extension to the filename
+            name = "%s-%s.mkv" % (name, cid)
 
             logging.info("Started downloading %s" % name)
             hikvisionapi.downloadRTSP(url,
