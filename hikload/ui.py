64bc109258e7493e4a0a2e7be566a6a614e7a59e
Stoica Tedy <stoicatedy@gmail.com>
2022-07-16 11:40:27 +0300

    fix: cleanup a lot of the UI code

--- hikload/ui.py
+++ hikload/ui.py
@@ -17,6 +17,7 @@ from .download import (create_folder_and_chdir, download_recording,
 
 log_stream = StringIO()
 
+
 class downloadThread(threading.Thread):
     def __init__(self, window: MainWindow, server: HikvisionServer, args):
         threading.Thread.__init__(self)
@@ -32,7 +33,7 @@ class downloadThread(threading.Thread):
 
     def run(self):
         logger = logging.getLogger('hikload')
-        logger.info("download thread started")
+        logger.info("Download thread started")
         logger.debug(f"{self.args=}")
 
         if self.args.debug:
@@ -43,14 +44,13 @@ class downloadThread(threading.Thread):
         if self.args.mock:
             recordings = search_for_recordings_mock(self.args)
         else:
-            recordings = search_for_recordings(self.args)
+            recordings = search_for_recordings(self.server, self.args)
         self.recordings = recordings
 
         self.window.ui.progressBar.setMaximum(len(recordings))
         self.window.ui.progressBar.setValue(0)
+        create_folder_and_chdir(self.args.downloads)
         original_path = os.path.abspath(os.getcwd())
-        if self.args.downloads:
-            create_folder_and_chdir(self.args.downloads)
 
         self.window.ui.treeWidget.clear()
         i = 0
@@ -64,13 +64,15 @@ class downloadThread(threading.Thread):
                 return
             self.window.ui.progressBar.setValue(self.finished)
 
-            download_recording(self.server, self.args, recordingobj, original_path)
+            download_recording(self.server, self.args,
+                               recordingobj, original_path)
             self.window.ui.treeWidget.takeTopLevelItem(0)
 
             self.finished += 1
         self.window.ui.progressBar.setValue(len(recordings))
         logger.info("Finished downloading all recordings")
         logger.info("download thread finished")
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, args=None):
@@ -84,12 +86,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.show()
         frameGm = self.frameGeometry()
-        screen = QtWidgets.QApplication.desktop().screenNumber(QtWidgets.QApplication.desktop().cursor().pos())
+        screen = QtWidgets.QApplication.desktop().screenNumber(
+            QtWidgets.QApplication.desktop().cursor().pos())
         centerPoint = QtWidgets.QApplication.desktop().screenGeometry(screen).center()
         frameGm.moveCenter(centerPoint)
         self.move(frameGm.topLeft())
 
-        server = HikvisionServer(self.args.server, self.args.username, self.args.password)
+        server = HikvisionServer(
+            self.args.server, self.args.username, self.args.password)
         self.downloadthread = downloadThread(self, server, args)
         self.downloadthread.start()
 
@@ -108,6 +112,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def reject(self):
         sys.exit(0)
 
+
 class ErrorDialog(QtWidgets.QMessageBox):
     def __init__(self, message):
         super(ErrorDialog, self).__init__()
@@ -115,6 +120,7 @@ class ErrorDialog(QtWidgets.QMessageBox):
         self.setWindowTitle("Error")
         self.setText("Error while executing the action")
         self.setInformativeText(message)
+
 
 class Startup(QtWidgets.QDialog):
     def __init__(self, parent=None, args=None):
@@ -124,9 +130,11 @@ class Startup(QtWidgets.QDialog):
         self.args = args
         self.populate_with_args(args)
 
-        self.ui.downloads_folder_button.clicked.connect(self.select_download_folder)
+        self.ui.downloads_folder_button.clicked.connect(
+            self.select_download_folder)
         self.ui.test_connection_button.clicked.connect(self.test_connection)
-        self.ui.start_downloading_button.clicked.connect(self.start_downloading)
+        self.ui.start_downloading_button.clicked.connect(
+            self.start_downloading)
 
         self.ui.start_date.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
         self.ui.end_date.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
@@ -139,10 +147,12 @@ class Startup(QtWidgets.QDialog):
         self.ui.downloads_folder.setText(args.downloads)
         self.ui.username.setText(args.username)
         self.ui.password.setText(args.password)
-        self.ui.folder_behavior.setCurrentIndex(list.index([None, 'onepercamera', 'oneperday', 'onepermonth', 'oneperyear'], args.folders))
+        self.ui.folder_behavior.setCurrentIndex(list.index(
+            [None, 'onepercamera', 'oneperday', 'onepermonth', 'oneperyear'], args.folders))
         if args.photos:
             self.ui.download_type.setCurrentIndex(1)
-        self.ui.video_format.setCurrentIndex(list.index(['mkv', 'mp4', 'avi'], args.videoformat))
+        self.ui.video_format.setCurrentIndex(
+            list.index(['mkv', 'mp4', 'avi'], args.videoformat))
         self.ui.start_date.setDateTime(args.starttime)
         self.ui.end_date.setDateTime(args.endtime)
         self.ui.debug.setChecked(bool(args.debug))
@@ -156,9 +166,11 @@ class Startup(QtWidgets.QDialog):
         self.args.downloads = self.ui.downloads_folder.text()
         self.args.username = self.ui.username.text()
         self.args.password = self.ui.password.text()
-        self.args.folders = [None, 'onepercamera', 'oneperday', 'onepermonth', 'oneperyear'][self.ui.folder_behavior.currentIndex()]
+        self.args.folders = [None, 'onepercamera', 'oneperday',
+                             'onepermonth', 'oneperyear'][self.ui.folder_behavior.currentIndex()]
         self.args.photos = self.ui.download_type.currentIndex() == 1
-        self.args.videoformat = ['mkv', 'mp4', 'avi'][self.ui.video_format.currentIndex()]
+        self.args.videoformat = ['mkv', 'mp4',
+                                 'avi'][self.ui.video_format.currentIndex()]
         self.args.starttime = self.ui.start_date.dateTime().toPyDateTime()
         self.args.endtime = self.ui.end_date.dateTime().toPyDateTime()
         self.args.debug = self.ui.debug.isChecked()
@@ -173,7 +185,8 @@ class Startup(QtWidgets.QDialog):
         return self.args
 
     def select_download_folder(self):
-        file = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory"))
+        file = str(QtWidgets.QFileDialog.getExistingDirectory(
+            self, "Select Directory"))
         self.ui.downloads_folder.setText(file)
 
     def start_downloading(self):
@@ -184,7 +197,8 @@ class Startup(QtWidgets.QDialog):
     def test_connection(self):
         if not self.args.mock:
             try:
-                server = HikvisionServer(self.server_ip.text(), self.username.text(), self.password.text())
+                server = HikvisionServer(self.ui.server_ip.text(
+                ), self.ui.username.text(), self.ui.password.text())
                 server.test_connection()
                 channelList = server.Streaming.getChannels()
                 self.ui.cameras.clear()
@@ -194,9 +208,10 @@ class Startup(QtWidgets.QDialog):
                     self.ui.cameras.insertTopLevelItems(i, [item])
                     i += 1
                 self.ui.cameras.selectAll()
-            except Exception as e:
-                ErrorDialog("Could not connect to the server").exec_()
-                logging.error(e)
+            except Exception as exc:
+                ErrorDialog(
+                    f"Could not connect to the server.\n{exc}").exec_()
+                logging.error(exc)
                 return
         else:
             self.ui.cameras.clear()
@@ -206,7 +221,8 @@ class Startup(QtWidgets.QDialog):
                 self.ui.cameras.insertTopLevelItems(i, [item])
                 i += 1
             self.ui.cameras.selectAll()
-        QtWidgets.QMessageBox.information(self, "Success", "Successfully connected to the server")
+        QtWidgets.QMessageBox.information(
+            self, "Success", "Successfully connected to the server")
         self.ui.start_downloading_button.setEnabled(True)
 
     def closeEvent(self, event):
@@ -234,6 +250,7 @@ def main(args=None):
     window = MainWindow(args)
     app.exec_()
     window.show()
+
 
 if __name__ == '__main__':
     main()
